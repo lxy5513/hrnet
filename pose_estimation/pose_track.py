0c5e3ed07001479c80570ea7f0238ce5c7f4c058
ferry <lxy5513@gmail.com>
2019-04-20 16:06:14 +0800

    根据相邻帧的OKS, 添加track moudle

---
+++ pose_estimation/pose_track.py
@@ -0,0 +1,205 @@
+'''
+使用yolov3作为pose net模型的前处理
+'''
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import os
+import pprint
+import ipdb;pdb=ipdb.set_trace
+import numpy as np
+from tqdm import tqdm
+from utilitys import plot_keypoint, PreProcess, plot_keypoint_track
+import time
+
+import torch
+import _init_paths
+from config import cfg
+import config
+from config import update_config
+
+from utils.transforms import *
+from lib.core.inference import get_final_preds
+import cv2
+import models
+from lib.detector.yolo.human_detector import main as yolo_det
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train keypoints network')
+    # general
+    parser.add_argument('--cfg',
+                        help='experiment configure file name',
+                        default='experiments/coco/hrnet/w32_256x192_adam_lr1e-3.yaml',
+                        #  default='experiments/coco/hrnet/w48_256x192_adam_lr1e-3.yaml',
+                        #  default='experiments/coco/hrnet/w32_384x288_adam_lr1e-3.yaml',
+                        type=str)
+
+    parser.add_argument('opts',
+                        help="Modify config options using the command-line",
+                        default=None,
+                        nargs=argparse.REMAINDER)
+
+    # philly
+    parser.add_argument('--modelDir',
+                        help='model directory',
+                        type=str,
+                        default='')
+    parser.add_argument('--logDir',
+                        help='log directory',
+                        type=str,
+                        default='')
+    parser.add_argument('--dataDir',
+                        help='data directory',
+                        type=str,
+                        default='')
+    parser.add_argument('--prevModelDir',
+                        help='prev Model directory',
+                        type=str,
+                        default='')
+
+    parser.add_argument("-i", "--video_input", help="input video file name", default="/home/xyliu/Videos/sports/dance.mp4")
+    parser.add_argument("-o", "--video_output", help="output video file name", default="output/output.mp4")
+
+    parser.add_argument('--camera', action='store_true')
+    parser.add_argument('--display', action='store_true')
+
+    args = parser.parse_args()
+    return args
+
+
+
+##### load model
+def model_load(config):
+    model = eval('models.'+config.MODEL.NAME+'.get_pose_net')(
+        config, is_train=False
+    )
+    model_file_name  = 'models/pytorch/pose_coco/pose_hrnet_w32_256x192.pth'
+    #  model_file_name  = 'models/pytorch/pose_coco/pose_hrnet_w48_256x192.pth'
+    #  model_file_name  = 'models/pytorch/pose_coco/pose_hrnet_w32_384x288.pth'
+    state_dict = torch.load(model_file_name)
+    from collections import OrderedDict
+    new_state_dict = OrderedDict()
+    for k, v in state_dict.items():
+        name = k # remove module.
+        #  print(name,'\t')
+        new_state_dict[name] = v
+    model.load_state_dict(new_state_dict)
+    model.eval()
+    return model
+
+def ckpt_time(t0=None, display=None):
+    if not t0:
+        return time.time()
+    else:
+        t1 = time.time()
+        if display:
+            print('consume {:2f} second'.format(t1-t0))
+        return t1-t0, t1
+
+
+###### 加载human detecotor model
+from lib.detector.yolo.human_detector import load_model as yolo_model
+human_model = yolo_model()
+
+def main():
+    previous_ids = 0
+    args = parse_args()
+    update_config(cfg, args)
+
+    if not args.camera:
+        # handle video
+        cam = cv2.VideoCapture(args.video_input)
+        video_length = int(cam.get(cv2.CAP_PROP_FRAME_COUNT))
+    else:
+        cam = cv2.VideoCapture(0)
+        video_length = 30000
+
+    ret_val, input_image = cam.read()
+    # Video writer
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    input_fps = cam.get(cv2.CAP_PROP_FPS)
+    out = cv2.VideoWriter(args.video_output,fourcc, input_fps, (input_image.shape[1],input_image.shape[0]))
+
+
+    #### load pose-hrnet MODEL
+    pose_model = model_load(cfg)
+    #  pose_model = torch.nn.DataParallel(pose_model, device_ids=[0,1]).cuda()
+    pose_model.cuda()
+
+    item = 0
+    prev_max = 0
+    for i in tqdm(range(video_length-1)):
+
+        x0 = ckpt_time()
+        ret_val, input_image = cam.read()
+
+        item = 0
+        try:
+            bboxs, scores = yolo_det(input_image, human_model)
+            # bbox is coordinate location
+            inputs, origin_img, center, scale = PreProcess(input_image, bboxs, scores, cfg)
+        except Exception as e:
+            print(e)
+            out.write(input_image)
+            cv2.namedWindow("enhanced",0);
+            cv2.resizeWindow("enhanced", 960, 480);
+            cv2.imshow('enhanced', input_image)
+            cv2.waitKey(2)
+            continue
+
+        with torch.no_grad():
+            # compute output heatmap
+            inputs = inputs[:,[2,1,0]]
+            output = pose_model(inputs.cuda())
+            # compute coordinate
+            preds, maxvals = get_final_preds(
+                cfg, output.clone().cpu().numpy(), np.asarray(center), np.asarray(scale))
+
+        from track_u import bipartite_matching_greedy, compute_pairwise_oks
+        kps_b = np.concatenate((preds, maxvals), 2)
+        box_b = bboxs[:preds.shape[0]]
+
+        if previous_ids == 0:
+            previous_ids = [j for j in range(len(preds))]
+
+        if i>0:
+            similarity_matrix = compute_pairwise_oks(kps_a, box_a, kps_b)
+            prev_ids, cur_ids = bipartite_matching_greedy(similarity_matrix)
+
+            print('previous frame boxes: ',previous_ids)
+            print(prev_ids, cur_ids)
+
+            cur_len = len(box_b)
+            cur_maps = -np.ones(shape=(cur_len,))
+            for pos, num in enumerate(cur_ids):
+                cur_maps[num] = previous_ids[prev_ids[pos]]
+
+            prev_max = max(max(previous_ids), prev_max)
+
+            for j in range(cur_len):
+                if cur_maps[j] == -1.:
+                    cur_maps[j] = prev_max + 1
+
+            previous_ids = cur_maps.astype(np.uint8).tolist()
+            print('after map: ', previous_ids)
+
+
+        # 作为下一次循环的上一帧
+        kps_a = kps_b.copy()
+        box_a = box_b.copy()
+
+        if i>0:
+            image = plot_keypoint_track(origin_img, preds, maxvals, box_a, previous_ids, 0.1)
+            out.write(image)
+        if args.display and i>0:
+            ########### 指定屏幕大小
+            cv2.namedWindow("enhanced", cv2.WINDOW_GUI_NORMAL);
+            cv2.resizeWindow("enhanced", 960, 480);
+            cv2.imshow('enhanced', image)
+            cv2.waitKey(3)
+
+if __name__ == '__main__':
+    main()
